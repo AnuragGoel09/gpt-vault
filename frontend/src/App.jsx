60f1d812729aba80edec91e11a3997b9bb575ee1
AnuragGoel09 <goelanurag2003@gmail.com>
2024-06-20 00:51:31 +0530

    add notfound page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Authenticated setLoader={setLoader}>
           {
             !loader && <Routes>
-              <Route path="/" element={<Home/>}/>
+              <Route exact path="/" element={<Home/>}/>
               <Route path="/login" element={<Login/>}/>
               <Route path="/signup" element={<Signup/>}/>
               <Route path="/dashboard" element={<Dashboard/>}/>
