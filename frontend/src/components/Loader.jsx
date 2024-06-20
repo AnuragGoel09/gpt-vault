0f4602306e2c0c91751c54d2fdb33a3115f087fe
AnuragGoel09 <goelanurag2003@gmail.com>
2024-06-20 16:27:54 +0530

    chnaging icon

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -5,6 +5,7 @@ export default function Loader() {
   return (
     <div className='h-screen w-screen fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
         <CircularProgress/>
+        <div>It might take a while due to free deployment of backend</div>
     </div>
   )
 }
